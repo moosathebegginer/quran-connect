4a25a1579aa79c33b6f4f8420aa026be0bc7398b
moosathebegginer <alnafayacademy777@gmail.com>
2025-04-09 13:40:20 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    setTimeout(() => {
+      loader.classList.add("fade-out");
+    }, 1500); // Delay before fade-out starts
+  });
+  
